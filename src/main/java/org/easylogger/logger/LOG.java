4195115997f416c16525fea2b27459c8bf8af5f2
Nikita Sirotenko <nick.sirotenko@yandex.ru>
2023-11-13 21:11:21 +0600

    Add time in logs

--- src/main/java/org/easylogger/logger/LOG.java
+++ src/main/java/org/easylogger/logger/LOG.java
@@ -1,7 +1,11 @@
 package org.easylogger.logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LOG implements Logger {
 
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
     private Logger logger;
 
     private LOG() {
@@ -9,32 +13,32 @@ public class LOG implements Logger {
 
     @Override
     public void debug(String msg) {
-        logger.debug(msg);
+        logger.debug(dtf.format(LocalDateTime.now()) + "\t"  + msg);
     }
 
     @Override
     public void info(String msg) {
-        logger.info(msg);
+        logger.info(dtf.format(LocalDateTime.now()) + "\t"  + msg);
     }
 
     @Override
     public void warn(String msg) {
-        logger.warn(msg);
+        logger.warn(dtf.format(LocalDateTime.now()) + "\t"  + msg);
     }
 
     @Override
     public void error(String msg) {
-        logger.error(msg);
+        logger.error(dtf.format(LocalDateTime.now()) + "\t"  + msg);
     }
 
     @Override
     public void critical(String msg) {
-        logger.critical(msg);
+        logger.critical(dtf.format(LocalDateTime.now()) + "\t"  + msg);
     }
 
     @Override
     public void fatal(String msg) {
-        logger.fatal(msg);
+        logger.fatal(dtf.format(LocalDateTime.now()) + "\t" + msg);
     }
 
     private static class SingletonHolder {
